f4a324b5cd6b5cf1241cffead453afeca0558b75
estepi <soyestepi@gmail.com>
2018-06-21 17:28:48 +0200

    reduced version of the scripts for the estimation of FDR. In includes default plots  printed in png format

--- scripts/functions_fdr_noMC.R
+++ scripts/functions_fdr_noMC.R
@@ -85,48 +85,6 @@ getPositiveValues<-function(ADJList){
   return(totalPos)
 }
 ######################################################################
-PositivesByCell<-function(TrueValuesMatrixList,
-                          RandomValuesMatrixList, cores)
-{
-  if(length(TrueValuesMatrixList) == length(RandomValuesMatrixList))
-  {
-    final<-vector()
-    for (i in 1:length(RandomValuesMatrixList))  {
-      ll<-mclapply(RandomValuesMatrixList[[i]],mc.cores = cores,
-                 function(x)
-                 {
-                   
-                   falsePos<- length(which(which(unlist(x)  >0  )  %in% which(unlist(TrueValuesMatrixList[[i]]) >0 )     ) )
-                   return(falsePos)
-                 }
-      )   
-      final<-c(final, ll)
-    }
-    return(final)
-  }
-  else
-  {
-    print("List are not of the same length")
-  }
-}
-#######################################################################
-randomRowsList<-function(mmList)
-{
-  rrowsList<-vector("list")
-  i=1
-  for(n in 5:nrow(mmList[[1]]) )
-  {
-    print(n)
-    res<-lapply(mmList,  randomRows,  n)
-    print(dim(res[[1]]))
-    cres<-lapply(res, CRobCor)
-    rrowsList[[i]] <- cres#lista de matrices
-    print(length(cres))
-    i <- i + 1
-  }
-  return(rrowsList)
-}
-#esto me da una lista de listas de matrices con 
 #tengo una lista de listas
 getEdgesBySample<-function(rrlist, rho)
 {
@@ -140,25 +98,6 @@ getEdgesBySample<-function(rrlist, rho)
   }
   return(RRList)
 }
-#############################################################
-getEdgesBySampleByRho<-function(rrlist, rho,  start, end , interval)
-{
-  SampleList<-vector("list", length(rrlist))
-  i=1
-  for (i in 1:length(rrlist))
-  {
-    RandomEdgesList<-getEdgesByRHO(rrlist[[i]], start, end, interval)
-    meanByRHO<-
-      
-    tpByho<-matrix(nrow=length(seq(0.3, 0.6,.02)),
-                   data=as.numeric(unlist(RandomEdgesList)),
-                   byrow = T)
-    
-    SampleList[[i]] <-  rowMeans(tpByho)
-    i <- i + 1
-  }
-  return(SampleList)
- }
 #############################################################
 getEdgesByRHOmc<-function(RList,start,end, interval, cores)
 {
@@ -187,4 +126,168 @@ getEdgesByRHO<-function(RList,start,end, interval)
   }
   return(MList)
 }
+#########################Plots###########################################
+plots<-function(
+  start,
+  end,
+  interval,
+  trueEdges,
+  trueMEAN,
+  randomEdges,
+  randomMEAN
+  )
+{
+ratioNumberOfEdges<-randomEdges/ trueEdges*100
+ratioPositiveValues<-randomMEAN/trueMEAN*100
+eje1<-seq(start,end,interval)
+###########################################################################################
+  file1<-paste(paste("NumberOfEdges_",paste(start,end,sep="-"), sep=""), ".png", sep="")
+  png(file1)
+  plot(eje1,
+       trueEdges, 
+       main="Number of edges real / random data vs rho" ,
+       type="l", 
+       xlab="rho", 
+       ylab="Number of edges",
+       col="blue",
+       ylim=c(min(trueEdges, randomEdges), max(trueEdges, randomEdges)),
+       xlim=c(start, end))
+      lines(eje1, randomEdges, col="red")
+      legend("topright", 
+         c("real data","random data"), lty = 1, col = c("blue","red"))
+  dev.off()
+message("Plot 1: Number of edges real / random data vs rho")
+###########################################################################################
+  file2<-paste(paste("RatioNumberOfEdges_",paste(start,end,sep="-"), sep=""), ".png", sep="")
+  png(file2)
+  plot(eje1, (randomEdges/trueEdges)*100, 
+       main="Ratio edges in random data vs real data",
+       type="l")
+  abline(h=c(0,5,10))
+  dev.off()
+message("Plot 2: Ratio edges in random data vs real data")
+###########################################################
+file3<-paste(paste("RealPositiveValues_",paste(start,end,sep="-"), sep=""), ".png", sep="")
+png(file3)
+plot(eje1,
+     trueMEAN, 
+     type="l", col="blue", 
+     main="Adj positive values and edges in real data", 
+     xlab="values", ylab="rho",
+     ylim=c(min(trueMEAN, trueEdges), max(trueMEAN, trueEdges)),
+     xlim=c(start, end))
+lines(eje1,trueEdges, col="red")
+legend("topright", 
+       c("edges","positives values"),
+       lty = 1, col = c("red","blue"))
+dev.off()
+message("Plot 3: Adj positive values and edges in real data")
+##############################################
+file4<-paste(paste("RandomPositiveValues_",paste(start,end,sep="-"), sep=""), ".png", sep="")
+png(file4)
+plot(eje1,randomMEAN, 
+     type="l", col="blue", 
+     main="Adj positive values and edges in random data",
+     ylim=c(min(randomMEAN, randomEdges), max(randomMEAN, randomEdges)),
+     xlim=c(start, end)
+)
+lines(eje1,randomEdges, col="red")
+legend("topright", c("edges","positives values"), lty = 1, col = c("red","blue"))
+dev.off()
+message("Plot 4: Adj positive values and edges in random data")
+##############################################
+file5<-paste(paste("Edges_vs_Positive_",paste(start,end,sep="-"), sep=""), ".png", sep="")
+png(file5)
+  plot(eje1,trueMEAN, 
+       type="l", col="blue", 
+       main="Adj Matrix positive values and edges in real and random data",
+       xlab="rho", 
+       ylab="values" ,
+       ylim=c(min(randomMEAN,trueMEAN, randomMEAN,randomEdges), 
+              max(randomMEAN,trueMEAN, randomMEAN,randomEdges)),
+       xlim=c(start, end))
+      lines(eje1,trueEdges, col="green")#t
+    lines(eje1,randomMEAN, type="l", col="red")#
+    lines(eje1,randomEdges, col="black")#
+    legend("topright", c("positives values real data",
+                       "edges real values",
+                       "positives values random data", 
+                       "edges random data"), 
+         lty = 1, 
+         col = c("blue", "green","red", "black"))
+dev.off()
+message("Plot 5: Adj Matrix positive values and edges in real and random data")
+#################################################  
+#ratios:
+file6<-paste(paste("Ratios_Edges_vs_Positives_",paste(start,end,sep="-"), sep=""), ".png", sep="")
+png(file6)
+plot(eje1,  
+     ratioNumberOfEdges, 
+     type="l", 
+     main="Ratios random/real", 
+     col="red", 
+     xlab="rho", 
+     ylab="ratio",
+     ylim=c(min(ratioNumberOfEdges, ratioPositiveValues), 
+            max(ratioNumberOfEdges, ratioPositiveValues)),
+     xlim=c(start, end))
+lines(eje1, ratioPositiveValues, col="blue", type="l") 
+legend("topright", 
+       c("ratio number of edges",
+         "ratio positive values"), 
+       lty = 1, 
+       col = c("red", "blue"))
+dev.off()
+message("Plot 6: Ratios random/real")
+}
+##################################################
+estimateFDR<-function(
+  inputM=sampleData,
+  NumRandomM=10,
+  start=0.3,
+  end=1,
+  interval=0.05)
+{
+  TrueList<-vector("list",1)
+  TrueList[[1]]<-sampleData
+  TrueList<-rep(TrueList, NumRandomM)
+  TPList<-lapply(TrueList, CRobCor)# 
+  
+  RandomList<-createRandomMatrix(TrueM = sampleData,  NumRandomM)
+  RList <- vector("list", length(RandomList))
+  RList<-lapply(RandomList, CRobCor)# paraleliza OK
+  TPList<-rep(TPList, NumRandomM)
+  RandomEdgesList<-getEdgesByRHO(RList, start, end, interval)
+  print(paste("Lenght of random edge list:",length(RandomEdgesList))) 
+  TrueEdgesList<-getEdgesByRHO(TPList, start, end, interval)
+  print(paste("Lenght of true edge list:", length(TrueEdgesList)   )  ) 
+  trueEdges<-unlist(lapply(TrueEdgesList, mean))
+  randomEdges<- unlist(lapply(RandomEdgesList, mean))
+  ##################################################
+  #Using adjacency matrix
+  TrueAdjMatrix<-getADJRHO(TPList, start, end , interval)
+  print(paste("Lenght of true adj matrix list", length(TrueAdjMatrix) )  ) 
+  RandomAdjMatrix<-getADJRHO(RList, start, end , interval)
+  message(paste("Lenght of random adj matrix list",  length(RandomAdjMatrix)  )  ) 
+  ##################################################
+  #from here divide by 2
+  tp<-unlist(getPositiveValues(TrueAdjMatrix))/2
+  trueValues<-matrix(nrow=length(seq(start, end, interval)), data=tp, byrow = T)
+  trueMEAN<-rowMeans(trueValues)
+  rr<-unlist(getPositiveValues(RandomAdjMatrix))/2
+  randomValues<-matrix(nrow=length(seq(start, end, interval)), data=rr, byrow = T)
+  randomMEAN<-rowMeans(randomValues)
+  ##################################################
+  #add an small correcction in order to avoid infinite values
+  message("Finish computation. Lets plot.")
+  plots( start,  end, interval, 
+         trueEdges=trueEdges+0.01,
+         trueMEAN=trueMEAN+0.01,
+         randomEdges=randomEdges+0.01,
+         randomMEAN=randomMEAN+0.01)#all together
+  ##################################################
+  
+  
+}
+
 
