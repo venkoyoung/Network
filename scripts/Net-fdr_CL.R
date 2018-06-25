80a2f4911c8a9ac6adfad05ed46e2078b3602a63
estepi <soyestepi@gmail.com>
2018-06-25 13:59:41 +0200

    for CL

---
+++ scripts/Net-fdr_CL.R
@@ -0,0 +1,41 @@
+#first generate data: TRUE and RANDOM DATA
+library(QUIC)
+library(igraph)
+library(optparse)
+
+option_list = list(
+  make_option(c("-f", "--file"), type="character", default=NULL, 
+              help="matrix file name", metavar="character"),
+  make_option(c("-r", "--random"), type="numeric", default="10", 
+              help="number of random matrix to test [default= %default]", metavar="character"),
+  make_option(c("-s", "--start"), type="numeric", default="0.3", 
+              help="value of rho to start scanning [default= %default]", metavar="character"),
+  make_option(c("-e", "--end"), type="numeric", default="0.8", 
+              help="value of rho to finish scanning [default= %default]", metavar="character"),
+  make_option(c("-i", "--interval"), type="numeric", default="0.05", 
+              help="intervale between each rho computation [default= %default]", metavar="character"),
+  make_option(c("-c", "--cores"), type="numeric", default="1", 
+              help="number of cores to use[default= %default]", metavar="character")
+  )
+
+opt_parser = OptionParser(option_list=option_list)
+opt = parse_args(opt_parser)
+
+if (is.null(opt$file)){
+  print_help(opt_parser)
+  stop("At least one argument must be supplied (input file).n", call.=FALSE)
+}
+
+##############################################
+t_Zvals<-read.table(opt$file)
+sampleData <- as.matrix(t_Zvals)
+print(dim(sampleData))
+NumRandomM<-opt$random
+start<-opt$start
+end<-opt$end
+interval<-opt$interval
+
+print(c(start,end, interval, NumRandomM))
+source("~/Dropbox (CRG ADV)/Personal_Estefania/Network/Network/scripts/CRobCor.R")
+source("~/Dropbox (CRG ADV)/Personal_Estefania/Network/Network/scripts/functions_fdr_noMC.R")
+estimateFDR(  inputM=sampleData,  NumRandomM,  start,  end,  interval)
